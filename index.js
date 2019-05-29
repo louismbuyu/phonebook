05f2d3904d95a312444c59e014f2cc0f8ccedb72
LouisM <louis_mbuyu@unigroup.com>
2019-05-29 09:54:54 -0500

    made changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    return res.json({message: "Hello World"});
+    return res.json({message: "Hello World 2"});
 });
 
 app.listen(5000);
