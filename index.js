9b14e3df75a0f4d5d589ff8c4500e33b2154d902
LouisM <louis_mbuyu@unigroup.com>
2019-05-29 09:57:52 -0500

    fix port bug

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get('/', (req,res) => {
     return res.json({message: "Hello World 2"});
 });
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
